94e278d37d8baebc76e84af66c73fc8ec609afbe
Yogu <info@yogularm.de>
2012-07-18 00:07:42 +0200

    Shoutbox implemented

--- overrides/scripts/mobile.js
+++ overrides/scripts/mobile.js
@@ -4,6 +4,8 @@
 		var main;
 		var page;
 		var menu;
+		var shoutbox = null;
+		var lastHash = location.hash;
 		function init() {
 			container = $('<div>').attr('id', 'container').appendTo($('body'));
 			$('body > *').not(container).appendTo(container);
@@ -11,19 +13,31 @@
 			main = $('<div>').attr('id', 'main').insertBefore($('.overall'));
 			page = $('<div>').attr('id', 'page').appendTo(main);
 			$('.overall, p.copyright').appendTo(page);
-			menu = $('<div>').attr('id', 'menu').appendTo(main);
+			// call it menu-container so that #menu does not scroll to this
+			menu = $('<div>').attr('id', 'menu-container').appendTo(main);
 			createMenu($('<ul>').appendTo(menu));
 			
 			var contentcover = $('<div>').addClass('cover').insertBefore(menu);
 			contentcover.click(hideMenu);
 
-			var menubutton = $('<a>').attr('id', 'menubutton').text('Menü').prependTo('.mainheader');
+			var menubutton = $('<a>').attr('id', 'menubutton').text('Menü').attr('href', '#menu').prependTo('.mainheader');
 			menubutton.click(toggleMenu);
+			
+			$(window).on('hashchange', function() {
+				if (lastHash == '#shoutbox' && location.hash != '#shoutbox')
+					hideShoutbox();
+				if (lastHash == '#menu' && location.hash != '#menu')
+					hideMenu();
+				lastHash = location.hash;
+			});
+			if (location.hash == '#shoutbox')
+				showShoutbox();
 		}
 		
 		function hideMenu() {
 			$('body').removeClass('menu-expanded');
 			page.css('min-height', '0');
+			location.hash = '';
 		}
 		
 		function showMenu() {
@@ -33,6 +47,7 @@
 			setTimeout(function() {
 				page.css('min-height', menu.height()+'px');
 			}, 0);
+			location.hash = '#menu';
 		}
 		
 		function toggleMenu() {
@@ -58,9 +73,11 @@
 					.text('Private Nachrichten')
 					.css('background-image', 'url(/graphics/my/pn_small.png)')
 					.attr('href', '/my.php');
-				$('<a>').appendTo($('<li>').appendTo(menu))
-					.text('Shoutbox')
-					.attr('href', '/shoutbox.php');
+				if ($('#sidebar_shoutbox').length > 0)
+					$('<a>').appendTo($('<li>').appendTo(menu))
+						.text('Shoutbox')
+						.attr('href', '#shoutbox')
+						.click(showShoutbox);
 			} else {
 				$('<a>').appendTo($('<li>').appendTo(menu))
 					.text('Login')
@@ -88,6 +105,29 @@
 					.text('Logout')
 					.css('background-image', 'url(/graphics/header/logout.png)')
 					.attr('href', $('.mainmenu a[href^="login.php?logout"]').attr('href'));
+		}
+		
+		function showShoutbox(e) {
+			if (!shoutbox && $('#sidebar_shoutbox').length == 0)
+				return;
+			
+			hideMenu();
+			if (shoutbox === null) {
+				shoutbox = $('#sidebar_shoutbox');
+				shoutbox.prependTo(main);
+				$('<a>').addClass('close-link').text('Schließen').click(hideShoutbox).attr('href', '#')
+					.appendTo(shoutbox.find('.sidebarheader'));
+			}
+			$('.overall').hide();
+			shoutbox.show();
+			location.hash = '#shoutbox';
+		}
+		
+		function hideShoutbox() {
+			var shoutbox = $('#sidebar_shoutbox');
+			shoutbox.hide();
+			$('.overall').show();
+			location.hash = '';
 		}
 
 		$(init);
